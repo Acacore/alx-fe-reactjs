2d6a639cd6582152778a5e1e388526ae3ebb99f4
Acacore <mensart81@outllook.com>
2025-11-03 22:20:47 +0000

    Inline style added

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function Header(){
+
+    
+    
+    return(
+        
+        <header>
+            <h1>My Favorite Cities</h1>
+        </header>
+     
+    )
+
+}
+export default Header;
