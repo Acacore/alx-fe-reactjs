845e033b35bc03bf3b894ab4f517b222993c542b
Acacore <mensart81@outllook.com>
2025-11-14 23:37:41 +0000

    File requied present

--- recipe-sharing-app/recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -1,21 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import AddRecipeForm from './components/AddRecipeForm'
-import useRecipeStore from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import { useRecipeStore } from './components/recipeStore';
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const recipes = useRecipeStore(state => state.recipes);
+  const [count, setCount] = useState(0);              
 
   return (
-   <div className="">
-    <AddRecipeForm/>
-    <useRecipeStore/>
-    
-   </div>
-  )
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
 }
 
 export default App
