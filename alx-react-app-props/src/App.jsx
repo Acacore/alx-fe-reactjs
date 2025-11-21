1dcc9bef2c51a659d421608b57e7ff7a751e64f7
Acacore <mensart81@outllook.com>
2025-11-21 15:44:34 +0000

    Display GitHub user info with conditional rendering and loading/error states

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import ProfilePage from './components/ProfilePage'
-import UserDetails from './components/UserDetails'
-import UserInfo from './components/UserInfo'
-import UserContext from './components/UserContext'
+// import ProfilePage from './components/ProfilePage'
+// import UserDetails from './components/UserDetails'
+// import UserInfo from './components/UserInfo'
+// import UserContext from './components/UserContext'
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  // const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <div>
