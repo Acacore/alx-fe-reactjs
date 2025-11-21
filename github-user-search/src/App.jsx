bf9fe91cf83dbdae0aa14f330b4763469cc18ada
Acacore <mensart81@outllook.com>
2025-11-21 14:40:42 +0000

    Add Header file in components directory

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Api from './services/Api'
+import Header from '../components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,8 @@ function App() {
 
   return (
     <div>
-     
+      <Header/>
+     <Api/>
         
     </div>
   )
