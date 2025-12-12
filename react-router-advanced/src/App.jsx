741b0466ddce6119ddf9e1e543a77b94883beac8
Elite81 <scophield@outlook.com>
2025-12-12 21:21:33 +0000

    fix: move BrowserRouter to App.jsx to satisfy ALX checker

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import Layout from './components/Layout'
 import ProtectedRoute from './components/ProtectedRoute'
@@ -16,29 +16,31 @@ import ProfileSettings from './pages/Profile/ProfileSettings'
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="login" element={<Login />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="blog/:postId" element={<PostDetail />} />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="login" element={<Login />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="blog/:postId" element={<PostDetail />} />
 
-          {/* Protected Profile with Nested Routes */}
-          <Route
-            path="profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
+            {/* Protected Profile with Nested Routes */}
+            <Route
+              path="profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<ProfileDetails />} />
+              <Route path="settings" element={<ProfileSettings />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
-    </AuthProvider>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
