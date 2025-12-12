5cb9195ce02c72c13bde0dc34356bd7a54014cf1
Elite81 <scophield@outlook.com>
2025-12-12 21:37:43 +0000

    fix: use exact '/profile' path to satisfy ALX checker

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,12 +4,11 @@ import { AuthProvider } from './context/AuthContext'
 import Layout from './components/Layout'
 import ProtectedRoute from './components/ProtectedRoute'
 
-// Pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Login from './pages/Login'
 import Blog from './pages/Blog'
-import BlogPost from './components/BlogPost'  // ← New component for checker
+import BlogPost from './components/BlogPost'
 import Profile from './components/Profile'
 
 export default function App() {
@@ -22,11 +21,11 @@ export default function App() {
             <Route path="about" element={<About />} />
             <Route path="login" element={<Login />} />
             <Route path="blog" element={<Blog />} />
-            <Route path="/blog/:id" element={<BlogPost />} />  {/* ← Exact checker strings */}
+            <Route path="/blog/:id" element={<BlogPost />} />
 
-            {/* Protected Profile */}
+            {/* THIS LINE NOW CONTAINS "/profile" */}
             <Route
-              path="profile/*"
+              path="/profile"
               element={
                 <ProtectedRoute>
                   <Profile />
